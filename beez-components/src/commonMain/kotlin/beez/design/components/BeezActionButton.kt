eae5ee9379ea106bd7d0564ace123354609c3a97
90ms <myungsung510@gmail.com>
2026-07-31 15:16:30 +0900

    fix: correct action button focus modifier import

--- beez-components/src/commonMain/kotlin/beez/design/components/BeezActionButton.kt
+++ beez-components/src/commonMain/kotlin/beez/design/components/BeezActionButton.kt
@@ -20,7 +20,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.onFocusChanged
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
